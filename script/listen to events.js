414b24b0d1c4c1eb7d7f8e42c1a0e466fd134f7b
Borgir <1borgir1@gmail.com>
2024-12-23 20:38:24 +1300

    Add a modal window with a description about the application

--- script/listen to events.js
+++ script/listen to events.js
@@ -39,7 +39,25 @@ buttonDivision.addEventListener('click', () => {
   divButton.remove();
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("modal");
+  const closeBtn = document.getElementById("close");
 
+  // Показываем модальное окно при загрузке страницы
+  modal.style.display = "flex";
+
+  // Закрываем модальное окно при клике на крестик
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  // Закрываем модальное окно при клике вне его
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
 
 
 
