832dd47a8c2467d1d192b50dac49c7bcf9bfa394
Borgir <1borgir1@gmail.com>
2025-06-01 13:23:43 +1200

    working on a modular window

--- script/addModalWindow.js
+++ script/addModalWindow.js
@@ -4,30 +4,27 @@ export function addModalWindow() {
   newModalWindow.innerHTML = `
   <div class="upload-container">
       <div class="preview" id="preview"></div>
-      <h1 id="upload">Motivational Picture</h1>
+      <h2 id="upload">Set a goal for the student</h2>
+      <p>Upload a motivational picture</p>
       <form id="uploadForm" class="uploadForm">
         <input type="file" id="fileInput" accept="image/*">
         <br>
         <!-- <button type="submit">Upload</button> -->
       </form>
+      <h2>Set the required number of coins</h2>
       <div class="upload-tasks">
         <p>Number of coins</p>
         <img src="./img/coin_pig.png" class="coin-pig">
         <input type="text" id="parantScore" >
         <button class ="button-apply">Apply</button>
-        
       </div>
       <p>one correctly solved example is 1 coin</p>
     </div>`
-
   newModalWindow.classList.add('modal-parant');
-
   // Находим контейнер, куда вставить
   const container = document.querySelector('body');
-
   // Вставляем элемент (например, в конец)
   container.appendChild(newModalWindow);
-
   //загрузка картинки
   let imgMotivational = ""
   fileInput.addEventListener('change', () => {
@@ -45,10 +42,14 @@ export function addModalWindow() {
       reader.readAsDataURL(file);
     }
   });
-
   //работаю с кнопкой "применить" родительское окно
   const buttonApply = document.querySelector(".button-apply");
   buttonApply.addEventListener("click", () => {
     creatingStudent(imgMotivational);
   })
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 }
